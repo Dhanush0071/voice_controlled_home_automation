9756fd48f2495f1264e571c975387ef50990a276
Dhanush <107381860+Dhanush0071@users.noreply.github.com>
2023-06-30 00:13:58 +0530

    Add files via upload

---
+++ IOT_HA.ino
@@ -0,0 +1,184 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include "DHT.h"
+
+//Relays for switching appliances
+#define Relay1            D6
+#define Relay2            D2
+#define Relay3            D1
+#define Relay4            D5
+#define DHTPIN            D7
+#define buzzer            D0
+#define S0                D3
+#define S1                D4
+#define analogpin         A0
+/************************* WiFi Access Point *********************************/
+#define WLAN_SSID       "VIRUS"
+#define WLAN_PASS       "mario123"
+/************************* Adafruit.io Setup *********************************/
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "Dhanush_5"
+#define AIO_KEY         "aio_qicJ127IkJpNBb6n5mo35L7Np2C8"
+
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+
+Adafruit_MQTT_Publish Humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidity");
+Adafruit_MQTT_Publish Temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+Adafruit_MQTT_Publish CO2 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/co2");
+Adafruit_MQTT_Publish Sound = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/sound");
+Adafruit_MQTT_Publish Light = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/light");
+
+
+Adafruit_MQTT_Subscribe Light1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay1");
+Adafruit_MQTT_Subscribe Fan1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay2");
+Adafruit_MQTT_Subscribe Light2 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay3");
+Adafruit_MQTT_Subscribe Fan2 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/relay4");
+
+
+#define DHTTYPE           DHT11     // DHT 11 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void MQTT_connect();
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(buzzer, OUTPUT);
+  pinMode(Relay1, OUTPUT);
+  pinMode(Relay2, OUTPUT);
+  pinMode(Relay3, OUTPUT);
+  pinMode(Relay4, OUTPUT);
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(A0, INPUT);
+
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  //Setting up DHT sensor
+  dht.begin();
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&Light1);
+  mqtt.subscribe(&Fan1);
+  mqtt.subscribe(&Light2);
+  mqtt.subscribe(&Fan2);
+}
+
+void loop() {
+  MQTT_connect();
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(20000))) {
+    if (subscription == &Light1) {
+      int Light1_State = atoi((char *)Light1.lastread);
+      digitalWrite(Relay1, Light1_State);
+      
+    }
+    if (subscription == &Light2) {
+      int Light2_State = atoi((char *)Light2.lastread);
+      digitalWrite(Relay2, Light2_State);
+    }
+    if (subscription == &Fan1) {
+      int Fan1_State = atoi((char *)Fan1.lastread);
+      digitalWrite(Relay3, Fan1_State);
+    }
+    if (subscription == &Fan2) {
+      int Fan2_State = atoi((char *)Fan2.lastread);
+      digitalWrite(Relay4, Fan2_State);
+      
+    }
+  }
+
+
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
+  int Value = analogRead(analogpin);
+  if (! CO2.publish(Value)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+  digitalWrite(S0, LOW);
+  digitalWrite(S1, HIGH);
+  int raw_sound = analogRead(analogpin);
+  Value = map(raw_sound,0,1024,0,100);
+  if (! Sound.publish(Value)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+  digitalWrite(S0, LOW);
+  digitalWrite(S1, LOW);
+  int raw_light = analogRead(analogpin);
+  int Value2 = map(raw_light,1024,0,0,100);
+  if (! Light.publish(Value2)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  
+  if (! Humidity.publish(h)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+  if (! Temperature.publish(t)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+
+}
+
+
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  digitalWrite(buzzer, HIGH);
+  delay(200);
+  digitalWrite(buzzer, LOW);
+  delay(200);
+  digitalWrite(buzzer, HIGH);
+  delay(200);
+  digitalWrite(buzzer, LOW);
+  delay(200);
+  while ((ret = mqtt.connect()) != 0) {
+    mqtt.disconnect();
+    delay(5000); 
+    retries--;
+    if (retries == 0) {
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+  digitalWrite(buzzer, HIGH);
+  delay(2000);
+  digitalWrite(buzzer, LOW);
+}
